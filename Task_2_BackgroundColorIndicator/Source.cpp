5344fc5d32f138657a22fea784b63057c8a81030
MadMks <mks59k@gmail.com>
2018-05-28 21:40:28 +0300

    Create task 2.

---
+++ Task_2_BackgroundColorIndicator/Source.cpp
@@ -0,0 +1,58 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+#include <vector>
+#include <time.h>
+#include <commctrl.h>
+using namespace std;
+
+#pragma comment(lib, "comctl32")
+
+
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpszCmdLine, int nCmdShow)
+{
+	srand(time(NULL));
+
+	MSG msg;
+	HWND hDialog = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG_Main), NULL, DlgProc);
+	ShowWindow(hDialog, nCmdShow);
+
+
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (uMessage)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		PostQuitMessage(0);
+		return TRUE;
+
+	case WM_INITDIALOG:
+
+
+
+		return TRUE;
+
+	case WM_COMMAND:
+
+
+		return TRUE;
+
+
+	}
+	return FALSE;
+}
