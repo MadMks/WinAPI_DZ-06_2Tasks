fc974015d2fbd7d7fdcfdd439460919209b3b5b6
MadMks <mks59k@gmail.com>
2018-05-29 07:35:05 +0300

    При движении на маленький шаг - на срабатывает ползунок.

--- Task_2_BackgroundColorIndicator/Source.cpp
+++ Task_2_BackgroundColorIndicator/Source.cpp
@@ -8,7 +8,11 @@ using namespace std;
 
 #pragma comment(lib, "comctl32")
 
+#define MIN 0
+#define MAX 255
 
+HWND hProgress;
+HWND hRedSlider, hGreenSlider, hBlueSlider;
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -33,6 +37,10 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpszCmdLine, int nCmd
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 {
+	TCHAR szCaption[25];
+	TCHAR szRedBuffer[4];
+	TCHAR szGreenBuffer[4];
+	TCHAR szBlueBuffer[4];
 
 	switch (uMessage)
 	{
@@ -43,14 +51,58 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
 
 	case WM_INITDIALOG:
 
+		hProgress = GetDlgItem(hWnd, IDC_PROGRESS);
+		hRedSlider = GetDlgItem(hWnd, IDC_SLIDER_Red);
+		hGreenSlider = GetDlgItem(hWnd, IDC_SLIDER_Green);
+		hBlueSlider = GetDlgItem(hWnd, IDC_SLIDER_Blue);
 
+		SendMessage(hRedSlider, TBM_SETRANGE, TRUE, MAKELPARAM(MIN, MAX));
+		SendMessage(hGreenSlider, TBM_SETRANGE, TRUE, MAKELPARAM(MIN, MAX));
+		SendMessage(hBlueSlider, TBM_SETRANGE, TRUE, MAKELPARAM(MIN, MAX));
 
 		return TRUE;
 
-	case WM_COMMAND:
+	case WM_HSCROLL:
 
+		if (LPARAM(wParam) == TB_THUMBTRACK
+			|| LPARAM(wParam) == TB_THUMBPOSITION
+			|| LPARAM(wParam) == TB_PAGEUP
+			|| LPARAM(wParam) == TB_PAGEDOWN
+			|| LPARAM(wParam) == TB_LINEUP
+			|| LPARAM(wParam) == TB_LINEDOWN
+			|| LPARAM(wParam) == TB_TOP
+			|| LPARAM(wParam) == TB_BOTTOM)
+		{
+			SendMessage(hProgress, PBM_SETBKCOLOR, 0, LPARAM(RGB(
+				SendMessage(hRedSlider, TBM_GETPOS, 0, 0),
+				SendMessage(hGreenSlider, TBM_GETPOS, 0, 0),
+				SendMessage(hBlueSlider, TBM_GETPOS, 0, 0)
+			)));
+
+			/*MessageBeep(1);
+			MessageBeep(1);*/
+			_itot(SendMessage(hRedSlider, TBM_GETPOS, 0, 0), szRedBuffer, 10);
+			_itot(SendMessage(hGreenSlider, TBM_GETPOS, 0, 0), szGreenBuffer, 10);
+			_itot(SendMessage(hBlueSlider, TBM_GETPOS, 0, 0), szBlueBuffer, 10);
+			lstrcat(szCaption, szRedBuffer);
+			lstrcat(szCaption, szGreenBuffer);
+			lstrcat(szCaption, szBlueBuffer);
+			SetWindowText(hWnd, szCaption);
+		}
 
 		return TRUE;
+
+	/*case TB_THUMBTRACK:
+
+		SendMessage(hProgress, PBM_SETBKCOLOR, 0, LPARAM(RGB(
+			SendMessage(hRedSlider, TBM_GETPOS, 0, 0),
+			SendMessage(hGreenSlider, TBM_GETPOS, 0, 0),
+			SendMessage(hBlueSlider, TBM_GETPOS, 0, 0)
+		)));
+
+		MessageBeep(1);
+
+		return TRUE;*/
 
 
 	}
